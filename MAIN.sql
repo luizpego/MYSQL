ae3bc1913f8fdb7367ecc385c22892597f0629d0
Luiz Andre Fernandes Pego <0118710@ifmg.edu.br>
2026-05-27 08:04:15 -0300

    Banco blog atualizado

--- MAIN.sql
+++ MAIN.sql
@@ -1,59 +1,157 @@
-CREATE DATABASE blog;
-USE blog;
+CREATE DATABASE livraria;
+USE livraria;
 
-CREATE TABLE usuario(
-    id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE editora (
+    id_editora INT PRIMARY KEY AUTO_INCREMENT,
+    nome_editora VARCHAR(100) NOT NULL,
+    cidade VARCHAR(50)
+);
+
+CREATE TABLE autores (
+    id_autor INT PRIMARY KEY AUTO_INCREMENT,
+    nome_autor VARCHAR(100) NOT NULL,
+    nacionalidade VARCHAR(50)
+);
+
+CREATE TABLE livros (
+    id_livro INT PRIMARY KEY AUTO_INCREMENT,
+    titulo VARCHAR(150) NOT NULL,
+    preco DECIMAL(10,2) NOT NULL,
+    ano_publicacao INT,
+    id_editora INT
+);
+
+CREATE TABLE livro_autor (
+    id_livro INT,
+    id_autor INT,
+    PRIMARY KEY (id_livro, id_autor)
+);
+
+CREATE TABLE clientes (
+    id_cliente INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
+    cpf VARCHAR(11) NOT NULL UNIQUE
+);
+
+CREATE TABLE pedidos (
+    id_pedido INT PRIMARY KEY AUTO_INCREMENT,
+    data_pedido DATE DEFAULT (CURRENT_DATE),
+    id_cliente INT
+);
+
+CREATE TABLE itens_pedido (
+    id_itens_pedido INT AUTO_INCREMENT,
+    id_pedido INT,
+    id_livro INT,
+    quantidade INT NOT NULL,
+    PRIMARY KEY (id_itens_pedido, id_pedido, id_livro)
+);
+
+CREATE TABLE categorias (
+    categoria_id INT PRIMARY KEY AUTO_INCREMENT,
+    nome_categoria VARCHAR(100) NOT NULL UNIQUE,
+    descricao TEXT
+);
+
+CREATE TABLE tags (
+    tag_id INT PRIMARY KEY AUTO_INCREMENT,
+    nome_da_tag VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE usuarios (
+    usuario_id INT PRIMARY KEY AUTO_INCREMENT,
+    nome_usuario VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP
+    is_admin BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE publicacoes(
+CREATE TABLE publicacoes (
     post_id INT PRIMARY KEY AUTO_INCREMENT,
-    titulo VARCHAR(200) NOT NULL,
-    conteudo TEXT NOT NULL,
-    data_publicacao DATETIME,
-    autor_id INT
+    titulo VARCHAR(150) NOT NULL,
+    conteudo TEXT,
+    categoria_id INT
 );
 
-CREATE TABLE comentario_post(
-    comentario_id INT PRIMARY KEY AUTO_INCREMENT,
-    texto_comentario TEXT NOT NULL,
-    data_comentario DATETIME,
-    usuario_id INT,
-    post_id INT
+CREATE TABLE publicacao_tag (
+    publicacao_id INT,
+    tag_id INT,
+    PRIMARY KEY (publicacao_id, tag_id)
 );
+
+CREATE TABLE curtidas (
+    curtida_id INT PRIMARY KEY AUTO_INCREMENT,
+    usuario_id INT,
+    post_id INT,
+    data_curtida DATETIME DEFAULT CURRENT_TIMESTAMP,
+    
+    UNIQUE (usuario_id, post_id),
+
+    FOREIGN KEY (usuario_id)
+    REFERENCES usuarios(usuario_id),
+
+    FOREIGN KEY (post_id)
+    REFERENCES publicacoes(post_id)
+);
+
+ALTER TABLE livros
+ADD CONSTRAINT fk_livro_editora
+FOREIGN KEY (id_editora)
+REFERENCES editora(id_editora);
+
+ALTER TABLE livro_autor
+ADD CONSTRAINT fk_livroautor_livro
+FOREIGN KEY (id_livro)
+REFERENCES livros(id_livro);
+
+ALTER TABLE livro_autor
+ADD CONSTRAINT fk_livroautor_autor
+FOREIGN KEY (id_autor)
+REFERENCES autores(id_autor);
+
+ALTER TABLE pedidos
+ADD CONSTRAINT fk_pedido_cliente
+FOREIGN KEY (id_cliente)
+REFERENCES clientes(id_cliente);
+
+ALTER TABLE itens_pedido
+ADD CONSTRAINT fk_itempedido_pedido
+FOREIGN KEY (id_pedido)
+REFERENCES pedidos(id_pedido);
+
+ALTER TABLE itens_pedido
+ADD CONSTRAINT fk_itempedido_livro
+FOREIGN KEY (id_livro)
+REFERENCES livros(id_livro);
 
 ALTER TABLE publicacoes
-ADD CONSTRAINT fk_post_usuario
-FOREIGN KEY (autor_id)
-REFERENCES usuario(id);
+ADD CONSTRAINT fk_publicacao_categoria
+FOREIGN KEY (categoria_id)
+REFERENCES categorias(categoria_id);
 
-ALTER TABLE comentario_post
-ADD CONSTRAINT fk_comentario_usuario
+ALTER TABLE publicacao_tag
+ADD CONSTRAINT fk_publicacaotag_publicacao
+FOREIGN KEY (publicacao_id)
+REFERENCES publicacoes(post_id);
+
+ALTER TABLE publicacao_tag
+ADD CONSTRAINT fk_publicacaotag_tag
+FOREIGN KEY (tag_id)
+REFERENCES tags(tag_id);
+
+ALTER TABLE curtidas
+ADD CONSTRAINT fk_curtida_usuario
 FOREIGN KEY (usuario_id)
-REFERENCES usuario(id);
+REFERENCES usuarios(usuario_id);
 
-ALTER TABLE comentario_post
-ADD CONSTRAINT fk_comentario_post
+ALTER TABLE curtidas
+ADD CONSTRAINT fk_curtida_post
 FOREIGN KEY (post_id)
 REFERENCES publicacoes(post_id);
 
-ALTER TABLE comentario_post
-ADD resumo VARCHAR(255);
+ALTER TABLE categorias
+MODIFY descricao TEXT NULL;
 
-ALTER TABLE usuario
-MODIFY nome VARCHAR(150);
+CREATE TABLE tabela_teste AS
+SELECT * FROM categorias;
 
-ALTER TABLE publicacoes
-ADD CONSTRAINT chk_titulo
-CHECK (LENGTH(titulo) > 5);
-
-ALTER TABLE usuario
-RENAME COLUMN data_cadastro TO data_de_criacao;
-
-ALTER TABLE comentario_postAS
-DROP COLUMN resumo;
-
-ALTER TABLE publicacoes
-RENAME TO publicacao;
+DROP TABLE tabela_teste;
